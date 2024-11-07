3e20b2503bdc4d1ac950147d1abd86dc52949d39
belay09 <belay1.sirak2.tsige3@gmail.com>
2024-11-07 19:11:34 +0300

    Update author name and email

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import "./assets/main.css";
-
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createMetaManager } from 'vue-meta';
 
 const app = createApp(App);
+const metaManager = createMetaManager();
 
 app.use(router);
+app.use(metaManager);
 
-app.mount("#app");
+app.mount('#app');
