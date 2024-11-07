c6c3e2f816dc31146492dcd26aaa95026c4bff86
belay09 <belay1.sirak2.tsige3@gmail.com>
2024-11-07 19:14:09 +0300

    Update main.js file

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import { createMetaManager } from 'vue-meta';
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
 const app = createApp(App);
-const metaManager = createMetaManager();
 
 app.use(router);
-app.use(metaManager);
 
-app.mount('#app');
+app.mount("#app");
