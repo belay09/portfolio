c62a92b873671d4379091076defb4f920b1c2d53
belay09 <belay1.sirak2.tsige3@gmail.com>
2024-11-08 14:16:33 +0300

    Refactor main.js: Remove code to hide loading spinner on app mount

--- src/main.js
+++ src/main.js
@@ -14,9 +14,3 @@ app.use(router);
 app.mount("#app");
 AOS.init(); // Initialize AOS
 
-window.addEventListener('load', () => {
-    const loader = document.getElementById('loader');
-    if (loader) {
-        loader.style.display = 'none';
-    }
-});
